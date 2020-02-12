3cd639d062f0e695b16cc2242f591cb5fd872d38
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2020-02-12 17:07:35 +0000

    Finished BMI calculator

--- BMI_Calculator/BMI_Calculator/BMI_Calculator/MainPage.xaml.cs
+++ BMI_Calculator/BMI_Calculator/BMI_Calculator/MainPage.xaml.cs
@@ -13,11 +13,72 @@ namespace BMI_Calculator
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        enum entrySource
         {
-            InitializeComponent();
+            weight,
+            height
         }
 
 
+
+        private BMIModel model = new BMIModel();
+        public MainPage()
+        {
+            InitializeComponent();
+            BmiLabel.IsVisible = false;
+            OutputLabel.IsVisible = false;
+        }
+
+        private async Task SyncViewAndModelAsync(entrySource src, string newValueAsString)
+        {
+            bool success;
+            string errorString;
+
+            if (src == entrySource.height)
+            {
+                success = model.SetHeightAsString(newValueAsString, out errorString);
+                HeightErrorLabel.IsVisible = !success;
+            }
+            else
+            {
+                success = model.SetWeightAsString(newValueAsString, out errorString);
+                WeightErrorLabel.IsVisible = !success;
+            }
+
+            if (model.BMIValue != null)
+            {
+                BmiLabel.IsVisible = true;
+                OutputLabel.IsVisible = true;
+                OutputLabel.Text = string.Format("{0:f1}", model.BMIValue);
+
+            }
+            else
+            {
+                BmiLabel.IsVisible = false;
+                OutputLabel.IsVisible = false;
+
+            }
+            if (!success)
+            {
+                await GiveFeedback(errorString);
+            }
+        }
+
+        private async Task GiveFeedback(string messageString)
+        {
+            ErrorLabel.Text = messageString;
+            await ErrorLabel.FadeTo(1.0, 500);
+            await Task.Delay(2000);
+            await ErrorLabel.FadeTo(0.0, 500);
+        }
+        private async void Handle_HeightAsync(object sender, TextChangedEventArgs e)
+        {
+            await SyncViewAndModelAsync(entrySource.height, e.NewTextValue);
+
+        }
+        private async void Handle_WeightAsync(object sender, TextChangedEventArgs e)
+        {
+            await SyncViewAndModelAsync(entrySource.weight, e.NewTextValue);
+        }
     }
 }
