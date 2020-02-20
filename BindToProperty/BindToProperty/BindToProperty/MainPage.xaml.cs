26dd3710029a5376e7ea7fc7e1af510a16145ae3
dsmale <daniel.smale@students.plymouth.ac.uk>
2020-02-20 09:52:20 +0000

    Successfully binded a label to a property

---
+++ BindToProperty/BindToProperty/BindToProperty/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindToProperty
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        Model dataModel = new Model();
+        int count = 0;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MyLabel.BindingContext = dataModel;
+
+            MyLabel.SetBinding(Label.TextProperty, "NumberAsString", BindingMode.OneWay);
+
+        }
+
+        private void MyButton_Clicked(object sender, EventArgs e)
+        {
+            count += 1;
+            var str = $"{count}";
+            dataModel.NumberAsString = str;
+        }
+    }
+}
---
+++ BindToProperty/BindToProperty/BindToProperty/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace BindToProperty
+{
+    class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string _NumberAsString;
+
+        public string NumberAsString
+        {
+            get => _NumberAsString;
+            set
+            {
+                if (!value.Equals(_NumberAsString))
+                {
+                    _NumberAsString = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("NumberAsString"));
+                    }
+                }
+            }
+        }
+
+    }
+}
