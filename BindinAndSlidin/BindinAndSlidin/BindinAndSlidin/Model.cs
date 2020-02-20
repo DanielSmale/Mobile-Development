6ac84dd4dd73b01b4737afd37466b3ae001ccf83
dsmale <daniel.smale@students.plymouth.ac.uk>
2020-02-20 10:57:10 +0000

    Binders on slider objects need to finish

---
+++ BindinAndSlidin/BindinAndSlidin/BindinAndSlidin/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace BindinAndSlidin
+{
+    class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _sliderValue;
+
+        public int sliderValue
+        {
+            set
+            {
+
+            }
+            get
+            {
+
+            }
+        }
+
+    }
+}
